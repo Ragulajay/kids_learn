80a5ebe68838bd2db83216282848c1a3a9995b7f
Ragulajay <ragul321@gmail.com>
2024-02-16 17:53:57 +0530

    fits commit

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
+urlpatterns=[
+    path('',views.all_category,name='home'),
+    path('all-news',views.all_news,name='all-news'),
+    path('detail/<int:id>',views.detail,name='detail'),
+    path('all-category',views.all_category,name='all-category'),
+    path('category/<int:id>',views.category,name='category'),
+    path('game/', views.game_view, name='game'),
+    path('eng/', views.spl,name='eng'),
+    path('mat/', views.mat,name='mat'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
